f103ad6f0a3d3b9732186e7edf43ade5cfe602ee
Guillaume-Lachapelle <guillaume2.lachapelle@gmail.com>
2024-01-30 16:15:04 -0500

    Get user for header nav

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewChecked {
+  authority!: string;
+  myUser!: any;
 
+  constructor(
+    public authService: AuthService,
+    private Acrouter: ActivatedRoute
+  ) {}
+
+  ngAfterViewChecked() {
+    this.myUser = this.authService.getUser();
+    const type = this.Acrouter.snapshot.params['type'];
+    if (type) {
+      this.authority = type;
+    }
+    if (this.myUser) {
+      this.authority = this.myUser.Authority;
+    }
+  }
 }
