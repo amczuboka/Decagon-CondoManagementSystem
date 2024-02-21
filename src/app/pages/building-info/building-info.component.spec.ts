41c7be02e26d9c092afd09881dc4567180464e90
Guillaume-Lachapelle <guillaume2.lachapelle@gmail.com>
2024-02-21 17:06:55 -0500

    Added imports inside the test
    Co-Authored-By: Oliver Vilney <96556635+Holyvert@users.noreply.github.com>
    Co-Authored-By: KarinaSandur <94981080+KarinaSandur@users.noreply.github.com>

--- src/app/pages/building-info/building-info.component.spec.ts
+++ src/app/pages/building-info/building-info.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BuildingInfoComponent } from './building-info.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { AppModule } from 'src/app/app.module';
 
 describe('BuildingInfoComponent', () => {
   let component: BuildingInfoComponent;
@@ -8,6 +10,7 @@ describe('BuildingInfoComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [ MatTabsModule, AppModule ],
       declarations: [BuildingInfoComponent]
     });
     fixture = TestBed.createComponent(BuildingInfoComponent);
