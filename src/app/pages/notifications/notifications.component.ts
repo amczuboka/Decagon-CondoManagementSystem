59eca5dca611ff95bebd6cc4eede0788354a7558
Guillaume-Lachapelle <guillaume2.lachapelle@gmail.com>
2024-02-26 18:09:09 -0500

    Added table that displays the notifications in notifications page
    Linked to #52 and #53

--- src/app/pages/notifications/notifications.component.ts
+++ src/app/pages/notifications/notifications.component.ts
@@ -11,25 +11,42 @@ import { UserService } from 'src/app/services/user.service';
 export class NotificationsComponent {
   authority!: string;
   myUser!: any;
-  displayedColumns: string[] = ['message', 'date', 'SenderId', 'actions'];
+  displayedColumns: string[] = [];
   dataSource: any = [];
 
   constructor(
     public authService: AuthService,
     public userService: UserService
-  ) {}
+  ) {
+    this.updateDisplayedColumns();
+    window.addEventListener('resize', () => {
+      this.updateDisplayedColumns();
+    });
+  }
+
+  updateDisplayedColumns() {
+    if (window.innerWidth <= 700) {
+      this.displayedColumns = ['message', 'date', 'actions'];
+    } else {
+      this.displayedColumns = ['message', 'date', 'SenderId', 'actions'];
+    }
+  }
 
   async ngOnInit() {
     await this.getUserData();
   }
 
-  async getUserData() {
+  async getUserData(): Promise<void> {
     return new Promise<void>((resolve) => {
       this.myUser = this.authService.getUser() as User;
       if (this.myUser) {
         const callback = (user: any) => {
           this.myUser = user;
-          this.dataSource = this.myUser.Notifications;
+          this.dataSource = this.myUser.Notifications.sort((a: any, b: any) => {
+            const dateA = new Date(a.Date).getTime();
+            const dateB = new Date(b.Date).getTime();
+            return dateB - dateA;
+          });
           resolve();
         };
 
@@ -49,7 +66,12 @@ export class NotificationsComponent {
   markAsRead(notification: any) {
     notification.New = false;
     this.myUser.Notifications = this.myUser.Notifications.map((n: any) =>
-      n.ID === notification.ID ? notification : n
+      n.Message === notification.Message &&
+      n.New === notification.New &&
+      n.Date === notification.Date &&
+      n.SenderId === notification.SenderId
+        ? notification
+        : n
     );
     this.userService.editUser(this.myUser.ID, this.myUser);
   }
@@ -57,7 +79,23 @@ export class NotificationsComponent {
   markAsUnread(notification: any) {
     notification.New = true;
     this.myUser.Notifications = this.myUser.Notifications.map((n: any) =>
-      n.ID === notification.ID ? notification : n
+      n.Message === notification.Message &&
+      n.New === notification.New &&
+      n.Date === notification.Date &&
+      n.SenderId === notification.SenderId
+        ? notification
+        : n
+    );
+    this.userService.editUser(this.myUser.ID, this.myUser);
+  }
+
+  deleteNotification(notification: any) {
+    this.myUser.Notifications = this.myUser.Notifications.filter(
+      (n: any) =>
+        n.Message !== notification.Message ||
+        n.New !== notification.New ||
+        n.Date !== notification.Date ||
+        n.SenderId !== notification.SenderId
     );
     this.userService.editUser(this.myUser.ID, this.myUser);
   }
