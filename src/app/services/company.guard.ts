040fbd486f81bddb447a56425524f01611f3db5c
Nicholas Piperni <36056036+npiperni@users.noreply.github.com>
2024-02-27 14:58:22 -0500

    Company User Guard

---
+++ src/app/services/company.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CompanyGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const user = this.authService.getUser();
+    if (user && user.photoURL === 'Company') {
+      return true;
+    } else {
+      this.router.navigate(['']);
+      return false;
+    }
+  }
+} 
