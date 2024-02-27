c3ec8dd82a1b48e58b98e9df47e208b2423a8fb7
CarlaShawi <91223318+CarlaShawi@users.noreply.github.com>
2024-02-26 21:57:53 -0500

    Added path for individual condo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ import { VerifyEmailComponent } from './pages/login/verify-email/verify-email.co
 import { AuthguardGuard } from './services/auth.guard';
 import { UserProfileComponent } from './pages/user-profile/user-profile.component';
 import { BuildingInfoComponent } from './pages/building-info/building-info.component';
+import { IndividualCondoComponent } from './pages/individual-condo/individual-condo.component';
+import { CondoFeaturesComponent } from './pages/individual-condo/condo-features/condo-features.component';
+import { DescriptionComponent } from './pages/individual-condo/description/description.component';
+import { LocationComponent } from './pages/individual-condo/location/location.component';
 
 const routes: Routes = [
   { path: '', component: LandingComponent, canActivate: [AuthguardGuard] },
@@ -18,7 +22,11 @@ const routes: Routes = [
     component: UserProfileComponent,
     canActivate: [AuthguardGuard],
   },
-  { path: 'building-info', component: BuildingInfoComponent}
+  { path: 'individual-condo', component: IndividualCondoComponent },
+  { path: 'building-info', component: BuildingInfoComponent },
+  { path: 'condo-features', component: CondoFeaturesComponent },
+  { path: 'description', component: DescriptionComponent },
+  { path: 'location', component: LocationComponent },
 ];
 
 @NgModule({
