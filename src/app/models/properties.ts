7cba5c3dfd3ab371fe81bac9adca0318e9a10b90
CarlaShawi <91223318+CarlaShawi@users.noreply.github.com>
2024-02-26 21:27:58 -0500

    Added Condo and Building DTOs

---
+++ src/app/models/properties.ts
@@ -0,0 +1,103 @@
+export interface Building {
+    ID: string;
+    Year: number;
+    CompanyID: string;
+    Name: string;
+    Address: string;
+    Bookings: Booking[];
+    Description: string;
+    Parkings: ParkingSpot[];
+    Lockers: Locker[];
+    Condos: Condo[];
+    Picture: string;
+    Facilities: Facilities[];
+  }
+  
+  export interface BuildingDTO {
+    Year: number;
+    Name: string;
+    Address: string;
+  }
+  
+  export interface Condo {
+    ID: string;
+    Type: CondoType;
+    OccupantID: string;
+    UnitNumber: string;
+    Fee: number;
+    Picture: string;
+    Description: string;
+    NumberOfBedrooms: number;
+    NumberOfBathrooms: number;
+    Status: CondoStatus;
+    SquareFootage: number;
+  }
+  
+  export interface CondoDTO {
+    UnitNumber: string;
+    Fee: number;
+    Picture: string;
+    Description: string;
+    NumberOfBedrooms: number;
+    NumberOfBathrooms: number;
+    SquareFootage: number;
+  }
+  
+  export interface ParkingSpot {
+    ID: string;
+    OccupantID: string;
+    Number: string;
+    Status: ParkingLockerStatus;
+    ParkingType: ParkingType;
+    Fee: number;
+  }
+  
+  export interface Locker {
+    ID: string;
+    OccupantID: string;
+    Number: string;
+    Status: ParkingLockerStatus;
+    Height: number;
+    Width: number;
+    Length: number;
+    Fee: number;
+  }
+  
+  export enum CondoStatus {
+    Vacant = 'Vacant',
+    Owned = 'Owned',
+    Rented = 'Rented',
+  }
+  
+  export enum ParkingLockerStatus {
+    Available = 'Available',
+    Unavailable = 'Unavailable',
+  }
+  
+  export enum CondoType {
+    Sale = 'Sale',
+    Rent = 'Rent',
+  }
+  
+  export enum Facilities {
+    Gym = 'Gym',
+    Pool = 'Pool',
+    Spa = 'Spa',
+    Locker = 'Locker',
+    Parking = 'Parking',
+    Playground = 'Playground',
+    MeetingRoom = 'Meeting Room',
+  }
+  
+  export interface Booking {
+    ID: string;
+    Facility: Facilities;
+    UserID: string;
+    Date: Date;
+  }
+  
+  export enum ParkingType {
+    Standard = 'Standard',
+    Handicap = 'Handicap',
+  }
+  
