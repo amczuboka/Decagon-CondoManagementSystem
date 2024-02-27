4cffb0d0c62e54019d7df3879fd8d9cb5369a631
CarlaShawi <91223318+CarlaShawi@users.noreply.github.com>
2024-02-27 10:02:38 -0500

    Added conditional rendering to component and updated spec test

--- src/app/pages/individual-condo/description/description.component.spec.ts
+++ src/app/pages/individual-condo/description/description.component.spec.ts
@@ -1,17 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DescriptionComponent } from './description.component';
+import { CondoDTO } from '../../../models/properties';
 
 describe('DescriptionComponent', () => {
   let component: DescriptionComponent;
   let fixture: ComponentFixture<DescriptionComponent>;
+  let mockCondo: Partial<CondoDTO>; 
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       declarations: [DescriptionComponent]
-    });
+    }).compileComponents();
+
     fixture = TestBed.createComponent(DescriptionComponent);
     component = fixture.componentInstance;
+
+    // Initialize mock data
+    mockCondo = {
+      Description: 'Spacious condo with great amenities'
+    };
+    
+    component.condo = mockCondo as CondoDTO; 
     fixture.detectChanges();
   });
 
