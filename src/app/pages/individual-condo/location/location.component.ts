6a0bb2b28eafd8f859e1a70367fcaf5208e8e080
CarlaShawi <91223318+CarlaShawi@users.noreply.github.com>
2024-02-27 10:11:33 -0500

    Modified map component and spec test

--- src/app/pages/individual-condo/location/location.component.ts
+++ src/app/pages/individual-condo/location/location.component.ts
@@ -12,7 +12,7 @@ export class LocationComponent implements OnInit {
   @Input() address!: string;
 
   constructor(private http: HttpClient) {}
-  
+
   ngOnInit() {
     if (this.address) {
       this.geocodeAddress();
@@ -20,30 +20,42 @@ export class LocationComponent implements OnInit {
   }
 
   geocodeAddress() {
-    const accessToken = 'pk.eyJ1IjoiY2FybGFzaGF3aSIsImEiOiJjbHN5dzBzMXAwaG1kMmtyb3pocnBvZzBlIn0.4k81B20yAJlUaU3hWCiCpQ'; 
-    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(this.address)}.json?access_token=${accessToken}`;
+    const accessToken =
+      'pk.eyJ1IjoiY2FybGFzaGF3aSIsImEiOiJjbHN5dzBzMXAwaG1kMmtyb3pocnBvZzBlIn0.4k81B20yAJlUaU3hWCiCpQ';
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+      this.address
+    )}.json?access_token=${accessToken}`;
 
-    this.http.get<any>(url).subscribe(data => {
-      if (data && data.features && data.features.length > 0) {
-        const coordinates = data.features[0].center;
-        this.initMap(coordinates);
+    this.http.get<any>(url).subscribe(
+      (data) => {
+        if (data && data.features && data.features.length > 0) {
+          const coordinates = data.features[0].center;
+          this.initMap(coordinates);
+        } else {
+          console.error('No coordinates found for the given address.');
+        }
+      },
+      (error) => {
+        console.error('Geocoding error:', error);
       }
-    }, error => {
-      console.error('Geocoding error:', error);
-    });
+    );
   }
 
   initMap(coordinates: [number, number]) {
-    mapboxgl.accessToken = 'pk.eyJ1IjoiY2FybGFzaGF3aSIsImEiOiJjbHN5dzBzMXAwaG1kMmtyb3pocnBvZzBlIn0.4k81B20yAJlUaU3hWCiCpQ'; 
+    if (!mapboxgl || !mapboxgl.Map || !mapboxgl.Marker) {
+      console.error('mapboxgl is not properly initialized.');
+      return;
+    }
+
+    mapboxgl.accessToken =
+      'pk.eyJ1IjoiY2FybGFzaGF3aSIsImEiOiJjbHN5dzBzMXAwaG1kMmtyb3pocnBvZzBlIn0.4k81B20yAJlUaU3hWCiCpQ';
     const map = new mapboxgl.Map({
-      container: 'map', 
-      style: 'mapbox://styles/mapbox/streets-v11', 
-      center: coordinates, 
-      zoom: 14 
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: coordinates,
+      zoom: 14,
     });
 
-    new mapboxgl.Marker()
-      .setLngLat(coordinates)
-      .addTo(map);
+    new mapboxgl.Marker().setLngLat(coordinates).addTo(map);
   }
 }
