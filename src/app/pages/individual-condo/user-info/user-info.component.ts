81d4b5b1faf16303449ce9492789b94fa59bca1a
CarlaShawi <91223318+CarlaShawi@users.noreply.github.com>
2024-03-17 22:12:51 -0400

    Updated user-info component

--- src/app/pages/individual-condo/user-info/user-info.component.ts
+++ src/app/pages/individual-condo/user-info/user-info.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { UserDTO } from '../../../models/users';
+import { Condo } from '../../../models/properties';
+
 @Component({
   selector: 'app-user-info',
   templateUrl: './user-info.component.html',
@@ -7,6 +9,7 @@ import { UserDTO } from '../../../models/users';
 })
 export class UserInfoComponent {
   @Input() userInfo!: UserDTO;
+  @Input() condo!: Condo;
 
   constructor() {}
 }
