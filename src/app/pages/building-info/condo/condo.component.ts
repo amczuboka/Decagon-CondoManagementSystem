937afcaaf6514d13ec91a90ca7e051c5daef8518
Guillaume-Lachapelle <guillaume2.lachapelle@gmail.com>
2024-03-19 15:23:39 -0400

    Added functionality to request ownership/rent
    Added functionality to request ownership/rent.
    Added NotificationType
    Notification is sent to company when clicking request button and banner pops up with confirmation.
    To be added later: Fix spec tests and cypress tests using notifications since its interface was updated. Add tests for this new functionality.
    Linked to #149

--- src/app/pages/building-info/condo/condo.component.ts
+++ src/app/pages/building-info/condo/condo.component.ts
@@ -2,6 +2,13 @@ import { Component, Input } from '@angular/core';
 import { Condo, Building } from 'src/app/models/properties';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
+import {
+  Authority,
+  Notification,
+  NotificationType,
+} from 'src/app/models/users';
+import { NotificationService } from 'src/app/services/notification.service';
 
 @Component({
   selector: 'app-condo',
@@ -15,11 +22,53 @@ export class CondoComponent {
   myUser!: any;
   authority!: string;
 
-  constructor(private router: Router, private authService: AuthService) {}
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private userService: UserService,
+    private notificationService: NotificationService
+  ) {}
 
-  requestOwnership() {
-    // Navigate to the 'key-registration' page
-    this.router.navigate(['/key-registration']);
+  async requestOwnership(item: Condo) {
+    // Create a new notification
+    const notification: Notification = {
+      Date: new Date().getTime(),
+      Message: `Request for ownership of unit ${item.UnitNumber} in ${this.building.Name}.`,
+      New: true,
+      SenderId: this.myUser.uid,
+      Type: NotificationType.OwnershipRequest,
+    };
+    console.log(this.building.CompanyID, notification);
+    console.log(item.UnitNumber, this.building.Name, this.myUser.uid);
+    await this.userService.sendNotificationToUser(
+      this.building.CompanyID,
+      Authority.Company,
+      notification
+    );
+    this.notificationService.sendNotification(
+      'Your request for ownership has been sent. You will be notified when it is approved.'
+    );
+  }
+
+  async requestRent(item: Condo) {
+    // Create a new notification
+    const notification: Notification = {
+      Date: new Date().getTime(),
+      Message: `Request for rental of unit ${item.UnitNumber} in ${this.building.Name}.`,
+      New: true,
+      SenderId: this.myUser.uid,
+      Type: NotificationType.RentRequest,
+    };
+    console.log(this.building.CompanyID, notification);
+    console.log(item.UnitNumber, this.building.Name, this.myUser.uid);
+    await this.userService.sendNotificationToUser(
+      this.building.CompanyID,
+      Authority.Company,
+      notification
+    );
+    this.notificationService.sendNotification(
+      'Your request for rental has been sent. You will be notified when it is approved.'
+    );
   }
 
   async ngOnInit() {
@@ -36,5 +85,4 @@ export class CondoComponent {
       this.authority = '';
     }
   }
-
 }
