9501eea300867d45a5a7581f3af35f525225602b
KarinaSandur <karinasd07@hotmail.com>
2024-03-18 20:26:39 -0400

    Fixed imports

--- src/app/pages/building-info/bookings/bookings.component.spec.ts
+++ src/app/pages/building-info/bookings/bookings.component.spec.ts
@@ -1,4 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppModule } from 'src/app/app.module';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatRadioModule} from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatChipsModule } from '@angular/material/chips';
 
 import { BookingsComponent } from './bookings.component';
 
@@ -8,6 +17,17 @@ describe('BookingsComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        AppModule,
+        MatDividerModule, 
+        MatRadioModule, 
+        MatCardModule, 
+        MatIconModule,
+        MatDatepickerModule,
+        MatNativeDateModule ,
+        MatButtonModule,
+        MatChipsModule
+      ],
       declarations: [BookingsComponent]
     });
     fixture = TestBed.createComponent(BookingsComponent);
