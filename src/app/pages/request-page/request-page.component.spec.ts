549999c24906cdbcddb67d150e5c83661631ba27
Nicholas Piperni <36056036+npiperni@users.noreply.github.com>
2024-03-20 03:49:42 -0400

    Created tests for request page front end

--- src/app/pages/request-page/request-page.component.spec.ts
+++ src/app/pages/request-page/request-page.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RequestPageComponent } from './request-page.component';
+import { AppModule } from 'src/app/app.module';
 
 describe('RequestPageComponent', () => {
   let component: RequestPageComponent;
@@ -8,6 +9,7 @@ describe('RequestPageComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [AppModule],
       declarations: [RequestPageComponent]
     });
     fixture = TestBed.createComponent(RequestPageComponent);
@@ -17,5 +19,28 @@ describe('RequestPageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('onSubmit() should open snack bar with error message', () => {
+    spyOn(component, 'openSnackBar');
+    component.onSubmit();
+    expect(component.openSnackBar).toHaveBeenCalledWith('You must select a request type.');
+  });
+
+  it('onSubmit() should open snack bar with success message', () => {
+    spyOn(component, 'openSnackBar');
+    component.requestForm.controls['RequestType'].setValue('Move In / Move Out');
+    component.onSubmit();
+    expect(component.openSnackBar).toHaveBeenCalledWith('Your request has been submitted.');
+  });
+
+  it('openSnackBar() should open snack bar with message', () => {
+    spyOn<any>(component['_snackBar'], 'open');
+    component.openSnackBar('test');
+    expect(component['_snackBar'].open).toHaveBeenCalledWith('test', '', {
+      duration: 5000,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+    });
   });
 });
