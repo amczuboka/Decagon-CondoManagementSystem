d0261bb23f87c1a0e3ba46c4bee49a1728b591b1
Guillaume-Lachapelle <guillaume2.lachapelle@gmail.com>
2024-03-20 21:41:46 -0400

    Added documentation comments in services
    Linked to #185

--- src/app/services/company.guard.ts
+++ src/app/services/company.guard.ts
@@ -4,12 +4,21 @@ import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CompanyGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
-  canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  /**
+   * Determines if a route can be activated based on the user's authority level.
+   *
+   * @returns An Observable, Promise, or immediate boolean value or UrlTree. Returns true if the user is a company, otherwise navigates to the root route and returns false.
+   */
+  canActivate():
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
     const user = this.authService.getUser();
     if (user && user.photoURL === 'Company') {
       return true;
@@ -18,4 +27,4 @@ export class CompanyGuard implements CanActivate {
       return false;
     }
   }
-} 
+}
