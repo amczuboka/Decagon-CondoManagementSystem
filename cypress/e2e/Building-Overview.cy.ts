81ffd742354023046fdb43ae9b611f3f88305c72
KarinaSandur <karinasd07@hotmail.com>
2024-03-26 16:03:41 -0400

    Company logs in and goes to properties page

--- cypress/e2e/Building-Overview.cy.ts
+++ cypress/e2e/Building-Overview.cy.ts
@@ -1,5 +1,29 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+/**
+ * Sign in as company user
+ * User goes to their properties page
+ * User publishes a new property with a description
+ * Sing out as company user
+ * Sign in as public user
+ * Click on new property and go to building-info page
+ */
+
+import { CondoType, ParkingType } from 'src/app/models/properties';
+import { CompanyDTO } from 'src/app/models/users';
+import { addCondo, addLocker, addParking, login } from './utils.cy';
+
+//Company logs in a creates new property
+describe('Company logs in', () => {
+  beforeEach(() => {
+    login('karinasd007@gmail.com', 'company_pass');
+    cy.url().should('eq', 'http://localhost:4200/');
+  });
+
+  it('Goes to my properties page', () => {
+    cy.get('.mat-toolbar.mat-toolbar-single-row').within(() => {
+      cy.wait(1000);
+      cy.contains('My Properties').click();
+      cy.wait(1000);
+    });
   })
+
 })
