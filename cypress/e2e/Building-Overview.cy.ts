c018a2372b4f74e00a356d4e1d2a706575dc7f3a
KarinaSandur <karinasd07@hotmail.com>
2024-03-27 12:24:58 -0400

    Fixed error with creating property

--- cypress/e2e/Building-Overview.cy.ts
+++ cypress/e2e/Building-Overview.cy.ts
@@ -1,25 +1,27 @@
 /**
- * Sign in as company user
- * User goes to their properties page
- * User publishes a new property with a description
- * Sing out as company user
+ * Company user logs in
+ * Company sser publishes a new property with a description 
  * Sign in as public user
- * Click on newly created property property
- * Go to building-overview page of newly created property
+ * Public user clicks on new property
+ * Public user navigates to building-overview tab
+ * Company user logs in
+ * Company user deleted newly created property
  */
 
-import { CondoType, ParkingType } from 'src/app/models/properties';
-import { CompanyDTO } from 'src/app/models/users';
-import { addCondo, addLocker, addParking, login } from './utils.cy';
 
-//Company user logs in and sees properties
-describe('Company user logs in', () => {
-  beforeEach(() => {
+import { CondoType, ParkingType } from 'src/app/models/properties';	
+import { CompanyDTO } from 'src/app/models/users';	
+import { addCondo, addLocker, addParking, login } from './utils.cy';	
+
+
+//Company logs in a creates new property
+describe('Company user logs in and sees properties', () => {
+  beforeEach(() => {	  
     login('rosef12997@hisotyr.com', '123456');
     cy.url().should('eq', 'http://localhost:4200/');
-  });
+  });	  
 
-  //Company sees properties
+
   it('Goes to my properties page', () => {
     cy.get('.mat-toolbar.mat-toolbar-single-row').within(() => {
       cy.wait(1000);
@@ -27,17 +29,18 @@ describe('Company user logs in', () => {
       cy.wait(1000);
     });
   })
+})  //end of first describe
 
-  //Company creates new property
-  it('Creates new property', async () => {
 
-    cy.get('.mat-toolbar.mat-toolbar-single-row').within(() => {
-      cy.wait(1000);
-      cy.contains('Add New Property').click();
-      cy.wait(1000);
-      cy.url().should('eq', 'http://localhost:4200/add-new-property');
-    });
+//Company user logs in and creates new property
+describe('Company user logs in and creates new property', () => {
+  beforeEach(() => {
+    login('rosef12997@hisotyr.com', '123456');
+    cy.url().should('eq', 'http://localhost:4200/');
+    cy.visit('/add-new-property');
+  });
 
+  it('Company creates new property',() => {
     cy.get('input[name="Name"]').type('Cypress Test 116');
     cy.get('input[name="Country"]').type('Sample Country');
     cy.get('input[name="State"]').type('Sample State');
@@ -48,59 +51,47 @@ describe('Company user logs in', () => {
     cy.get('input[name="ZipCode"]').type('H6J 7K8');
     cy.get('input[name="Year"]').type('2022');
     cy.get('textarea[name="Description"]').type('Sample Description');
+    cy.wait(2000);
     cy.get('input[name="Picture"]').selectFile('./cypress/e2e/TEST.png');
     cy.get('mat-checkbox[value="Gym"]').click();
     cy.get('mat-checkbox[value="Pool"]').click();
     cy.get('mat-checkbox[value="Spa"]').click();
     cy.get('mat-checkbox[value="Playground"]').click();
     cy.get('mat-checkbox[value="Meeting Room"]').click();
-    addCondo(CondoType.Rent);
+    cy.wait(1000);
     addCondo(CondoType.Sale);
+
+    //trying smt
+    // const addCondo = (Type: CondoType) => {
+    // cy.get('.button_setup').contains('Add Condo').click();                                             
+    // cy.get('input[name="Quantity"]').type('10');
+    // cy.get('input[name="UnitNumber"]').type('1A', { force: true });
+    // cy.get('input[name="SquareFootage"]').type('1000');
+    // cy.get('button[type="submit"]').click();
+    // }
+    //trying smt
+
+    cy.wait(1000);
     addLocker();
-    addParking(ParkingType.Standard);
+    cy.wait(1000);
+    // addParking(ParkingType.Standard);
+    // cy.wait(1000);
     addParking(ParkingType.Handicap);
     cy.get('button[type="submit"]').click();
     cy.get('.loading-indicator').should('exist');
     cy.wait(5000);
   });
-
-})  //end of first describe
-
-
-
-//Public user logs in
-describe('Public user logs in', () => {
-  beforeEach(() => {
-    cy.wait(5000);
-    login('karinasd07@hotmail.com', 'public_pass');
-    cy.url().should('eq', 'http://localhost:4200/');
-  });
-
-  
-  it('Public user clicks on newly created property', () => {
-    cy.wait(5000);
-    cy.get('.BuildingDiv').within(() => {
-      cy.wait(5000);
-      cy.contains('.name', 'Cypress Test 116')
-        .parents('.card-content') // Navigate to the parent container of the building
-        .within(() => {
-          cy.get('#view_btn').click(); // Find and click the "View" button
-        });
-    });
-  })
-  
-
 });
 
-//Company user logs in and creates new property
-describe('Company user logs in again', () => {
-  beforeEach(() => {
+//Company logs in and deletes newly created property
+describe('Company user logs in and deletes property', () => {
+  beforeEach(() => {	  
     login('rosef12997@hisotyr.com', '123456');
+    cy.wait(5000);
     cy.url().should('eq', 'http://localhost:4200/');
-  });
+  });	  
 
-  it('Delete newly created property', () => {
-    //Deletes all properties from current user
+  it('Deletes newly created property', () => {
     cy.window().then(async (win) => {
       const currentUser = (win as any).authService.getUser();
       const user = (await (win as any).userService.getCompanyUser(
@@ -112,7 +103,5 @@ describe('Company user logs in again', () => {
       });
       await Promise.all(promiseDelete);
     });
-   
-  }) 
-
-});
+  })
+})  //end of first describe
