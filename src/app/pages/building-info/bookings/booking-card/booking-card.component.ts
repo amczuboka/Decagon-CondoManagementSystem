b4fce5e75bc3cbc59d4e9975680c08f859a2fbf9
Holyvert <96556635+Holyvert@users.noreply.github.com>
2024-04-13 22:21:34 -0400

    Update booking-card.component.ts
    Co-Authored-By: Karina Sanchez <94981080+KarinaSandur@users.noreply.github.com>

--- src/app/pages/building-info/bookings/booking-card/booking-card.component.ts
+++ src/app/pages/building-info/bookings/booking-card/booking-card.component.ts
@@ -5,6 +5,7 @@ import { AuthService } from 'src/app/services/auth.service';
 import { UserService } from 'src/app/services/user.service';
 import { BookingsService } from 'src/app/services/bookings.service';
 import { NotificationService } from 'src/app/services/notification.service';
+import { BuildingService } from 'src/app/services/building.service';
 
 @Component({
   selector: 'app-booking-card',
@@ -12,48 +13,51 @@ import { NotificationService } from 'src/app/services/notification.service';
   styleUrls: ['./booking-card.component.scss'],
 })
 export class BookingCardComponent {
-
   @Input() building!: Building;
 
   myUser!: any;
   userSubscription: Subscription = new Subscription();
   myBookings: Booking[] = [];
-/**
- * 
- * @param userService 
- * @param authService 
- * @param bookingsService 
- * @param notificationService 
- */
+  /**
+   *
+   * @param userService
+   * @param authService
+   * @param bookingsService
+   * @param notificationService
+   */
   constructor(
-    public userService: UserService, 
+    public userService: UserService,
     public authService: AuthService,
     public bookingsService: BookingsService,
     public notificationService: NotificationService,
+    public buildingService: BuildingService
   ) {}
- 
+
   async ngOnInit() {
-    this.myUser = await this.authService.getUser();
-    if (this.myUser) {
-      this.userService.subscribeToPublicUser(this.myUser.uid, (user) => {
-        this.myUser = user;
-        if (this.myUser) {
-          if (this.myUser.Bookings) this.myBookings = this.myUser.Bookings;
+    //Subscribe to the building with the building.ID for real time updates
+
+    this.buildingService.subscribeToBuildingById(this.building?.ID).subscribe({
+      next: async (Building: Building | null) => {
+        if (Building) {
+          this.myUser = await this.authService.getUser();
+          this.userService.subscribeToPublicUser(this.myUser?.uid, (user) => {
+            this.myUser = user;
+            this.myBookings = Building.Bookings.filter(
+              (booking: Booking) => booking.OccupantID === this.myUser.ID
+            );
+          });
         }
-      });
-    }
+      },
+    });
   }
 
-/**
- * 
- * @param bookingID 
- */
-  async deleteBooking(bookingID: string){
+  /**
+   *
+   * @param bookingID
+   */
+  async deleteBooking(bookingID: string) {
     const buildingID = this.building.ID;
-    await this.bookingsService.removeBooking(buildingID, bookingID)
-      this.notificationService.sendNotification(
-        'Booking successfully deleted!'
-      );
-   
+    await this.bookingsService.removeBooking(buildingID, bookingID);
+    this.notificationService.sendNotification('Booking successfully deleted!');
   }
 }
