d63b9355cc318e8a4c59d44115deb303ad3ca394
amczuboka <94947740+amczuboka@users.noreply.github.com>
2024-04-27 14:33:50 -0400

    Followed the steps on the deployment plan

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.decagon',
+  appName: 'decagon',
+  webDir: 'dist/decagon',
+  bundledWebRuntime: false,
+};
+
+export default config;
